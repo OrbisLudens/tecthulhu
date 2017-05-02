5b8e5d82eadd183f9fe40f3583104c5de9072813
Martin Brenner <martin@brenner.de>
2017-05-02 08:03:22 +0200

    added resonator and faction status display

--- prototype.py
+++ prototype.py
@@ -1,1 +1,63 @@
+#!/usr/bin/env python
 # a prototype portal simulator
+
+import time, sys
+from ws2801 import ws2801 
+LEDs=ws2801() # Set the name of our module 
+NUMBER_OF_PIXELS=13 # Set the number of pixels
+ledpixels = [0] * NUMBER_OF_PIXELS # set up the pixel array
+
+L0COLOR = [0,0,0]
+L1COLOR = [255,255,0]
+L2COLOR = [255,100,0]
+L3COLOR = [255,30,0]
+L4COLOR = [255,0,0]
+L5COLOR = [255,0,30]
+L6COLOR = [255,0,100]
+L7COLOR = [255,0,255]
+L8COLOR = [100,0,255]
+RCOLORS = [L0COLOR,L1COLOR,L2COLOR,L3COLOR,L4COLOR,L5COLOR,L6COLOR,L7COLOR,L8COLOR]
+
+def setresonator(index, level, energy) :
+	r = RCOLORS[level][0] * energy / 8000
+	g = RCOLORS[level][1] * energy / 8000
+	b = RCOLORS[level][2] * energy / 8000
+	LEDs.setpixelcolor(ledpixels, index, LEDs.Color(r, g, b))
+	
+FNCOLOR = [100,100,100]
+FECOLOR = [0,255,0]
+FRCOLOR = [0,0,255]
+FCOLORS = [FNCOLOR,FECOLOR,FRCOLOR]
+
+def setfaction(faction, energy) :
+	r = FCOLORS[faction][0] * energy / 100
+	g = FCOLORS[faction][1] * energy / 100
+	b = FCOLORS[faction][2] * energy / 100
+	LEDs.setpixelcolor(ledpixels, 8, LEDs.Color(r, g, b))
+	LEDs.setpixelcolor(ledpixels, 9, LEDs.Color(r, g, b))
+	LEDs.setpixelcolor(ledpixels, 10, LEDs.Color(r, g, b))
+	LEDs.setpixelcolor(ledpixels, 11, LEDs.Color(r, g, b))
+
+
+try: 
+	LEDs.cls(ledpixels)
+	time.sleep(0.5)
+
+	setfaction(0,100)
+	LEDs.writestrip(ledpixels)
+	time.sleep(1)
+
+	setfaction(1,100)
+
+	for i in range(0, 8):
+		setresonator(i, i+1, 8000)
+		LEDs.writestrip(ledpixels)
+      		time.sleep(0.5)
+
+
+	LEDs.cls(ledpixels)
+	time.sleep(0.5)
+
+except KeyboardInterrupt:
+	LEDs.cls(ledpixels) 
+	sys.exit(0)
